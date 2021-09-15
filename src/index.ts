94b65382b6fe2377db86a9a5ede0824061601370
Hugo Blanc <hugoblanc.blend@gmail.com>
2021-09-15 11:55:22 +0200

    release 0.1.2

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import { AbstractHubRepository } from "./abstract-hub-repository";
-import { AbstractList } from "./abstract.list";
-import { SpinalHubRepository } from "./decorators/repository";
-import { ListHubRepository } from "./list-hub-repository";
-import { SpinalWrapper } from "./spinal-wrapper";
+import { AbstractList } from './abstract.list';
+import { SpinalHubRepository } from './decorators/repository';
+import { HubRepository } from './hub-repository';
+import { ListHubRepository } from './list-hub-repository';
+import { SpinalWrapper } from './spinal-wrapper';
+
 export {
   AbstractList,
   SpinalWrapper,
   ListHubRepository,
-  AbstractHubRepository,
+  HubRepository,
   SpinalHubRepository,
 };
